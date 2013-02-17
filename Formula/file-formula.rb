b9060ac9af3c6da56e2beba69d327ed31d5f10cb
Latchezar Tzvetkoff <latchezar@tzvetkoff.net>
2013-02-17 19:57:49 +0200

    updated file to 5.12

--- Formula/file-formula.rb
+++ Formula/file-formula.rb
@@ -2,10 +2,10 @@ require 'formula'
 
 # "File" is a reserved class name
 class FileFormula < Formula
-  url 'ftp://ftp.astron.com/pub/file/file-5.11.tar.gz'
   homepage 'http://www.darwinsys.com/file/'
-  md5 '16a407bd66d6c7a832f3a5c0d609c27b'
-  head 'git://github.com/glensc/file.git'
+  url 'ftp://ftp.astron.com/pub/file/file-5.12.tar.gz'
+  sha256 '199d0dab2e32814cbced8dc30d3d2d5b7e45587598160a2c401358b573273d22'
+  version '5.12'
 
   def install
     system "./configure", "--disable-dependency-tracking",
