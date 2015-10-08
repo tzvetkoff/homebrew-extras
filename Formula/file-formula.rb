3089ff28f2c45554ec65407f271ecf6e131c7dbc
Latchezar Tzvetkoff <latchezar@tzvetkoff.net>
2015-10-08 12:10:50 +0300

    file 5.25

--- Formula/file-formula.rb
+++ Formula/file-formula.rb
@@ -3,9 +3,9 @@ require 'formula'
 # "File" is a reserved class name
 class FileFormula < Formula
   homepage 'http://www.darwinsys.com/file/'
-  url 'ftp://ftp.astron.com/pub/file/file-5.24.tar.gz'
+  url 'ftp://ftp.astron.com/pub/file/file-5.25.tar.gz'
   sha256 '802cb3de2e49e88ef97cdcb52cd507a0f25458112752e398445cea102bc750ce'
-  version '5.24'
+  version '5.25'
   patch :DATA
 
   def install
