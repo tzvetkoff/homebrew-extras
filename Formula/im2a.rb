76981a0af402140b4ea737e5481ef5b95e991305
Latchezar Tzvetkoff <latchezar@tzvetkoff.net>
2016-08-30 20:24:11 +0300

    imwa 0.4.2

--- Formula/im2a.rb
+++ Formula/im2a.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Im2a < Formula
   homepage 'https://github.com/tzvetkoff/im2a'
-  url 'https://github.com/tzvetkoff/im2a/archive/v0.4.0.tar.gz'
-  sha256 '088b3840f47993354ed248a45a57f8555fcb4e051912a57fb477448713e525f4'
-  version '0.4.0'
+  url 'https://github.com/tzvetkoff/im2a/archive/v0.4.2.tar.gz'
+  sha256 'f3204cac793d3be9f1e6b31f2d26abb1b25a130dc56df2d1d1321780b2d76e2f'
+  version '0.4.2'
 
   head 'https://github.com/tzvetkoff/im2a/archive/master.tar.gz'
 
