992343f593170847645ffe6f3188fafeb51fc9e7
Latchezar Tzvetkoff <latchezar@tzvetkoff.net>
2018-02-18 02:41:34 +0200

    im2a 0.6.0

--- im2a.rb
+++ im2a.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Im2a < Formula
   homepage 'https://github.com/tzvetkoff/im2a'
-  url 'https://github.com/tzvetkoff/im2a/archive/v0.5.3.tar.gz'
-  sha256 'e5348787201a98ff7df78fcab4972c5a1e820363080a241977885fd5d14603bc'
-  version '0.5.3'
+  url 'https://github.com/tzvetkoff/im2a/archive/v0.6.0.tar.gz'
+  sha256 '8e642c54ce4f7af3b25d733cfe6e59a16d81321489694d61e0e4e152707dc496'
+  version '0.6.0'
 
   head 'https://github.com/tzvetkoff/im2a/archive/master.tar.gz'
 
